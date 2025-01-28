62b0dbb76ae50059254b5e1f4afc84f024c2b2db
AnilTuraga <31271852+Anilturaga@users.noreply.github.com>
2025-01-28 20:18:06 +0000

    added favicon support, document title and removed lucide icons

--- main.py
+++ main.py
@@ -13,6 +13,12 @@ from pages.console import console_component, console_page
 # Set up the app, including daisyui and tailwind for the chat component
 hdrs = (
     # Script(src="https://cdn.tailwindcss.com"),
+    # favicon
+    Link(
+        rel="icon",
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🗂️</text></svg>",
+        type="image/svg+xml",
+    ),
     Script(
         src="https://cdn.tailwindcss.com?plugins=typography,aspect-ratio,container-queries"
     ),
@@ -21,22 +27,22 @@ hdrs = (
         href="https://cdn.jsdelivr.net/npm/daisyui@4.12.10/dist/full.min.css",
     ),
     # icons
-    Script(src="https://unpkg.com/lucide@latest"),
-    Script(
-        """
-        setTimeout(() => {
-            lucide.createIcons();
-        }, 50);
-        """
-    ),
+    # Script(src="https://unpkg.com/lucide@latest"),
+    # Script(
+    #     """
+    #     setTimeout(() => {
+    #         lucide.createIcons();
+    #     }, 50);
+    #     """
+    # ),
     Script(
         """
     tailwind.config = {
       daisyui: {
         themes: ["cupcake","black","business","dim","corporate","night","nord","dracula", "lofi"],
       },
-
     };
+    document.title ="FastHTML Template";
     """
     ),
     MarkdownJS(),
